0f41d52709719dac059a831a28cc2b98e2198dca
carpenter-js <carpenterbarrett@gmail.com>
2018-11-21 18:50:01 -0700

    added folders table and router;

--- db/noteful-app.sql
+++ db/noteful-app.sql
@@ -1,25 +1,42 @@
+-- psql -U dev -d dev-restaurants-app -f ./db/noteful-app.sql
 DROP TABLE IF EXISTS notes;
-SELECT CURRENT_DATE;
+DROP TABLE IF EXISTS folders;
+-- SELECT CURRENT_DATE;
+
+CREATE TABLE folders (
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+ALTER SEQUENCE folders_id_seq RESTART WITH 100;
+
+INSERT INTO folders (name) VALUES
+  ('Archive'),
+  ('Drafts'),
+  ('Personal'),
+  ('Work');
 
 CREATE TABLE notes (
-    id serial PRIMARY KEY,
-    title text NOT NULL,
-    content text NOT NULL,
-    created timestamp default CURRENT_TIMESTAMP
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  content text,
+  created timestamp DEFAULT now(),
+  folder_id int REFERENCES folders(id) ON DELETE SET NULL
 );
 
 ALTER SEQUENCE notes_id_seq
 RESTART WITH 1000;
 
 INSERT INTO notes 
-(title, content)
+(title, content, folder_id)
 VALUES 
-('5 life lessons learned from cats', 'Lorem ipsum dolor sit amet.'),
-('What the government doesn''t want you to know about cats', 'They''re cute!'),
-('The most boring article about cats you''ll ever read', 'Boring article'),
-('7 things lady gaga has in common with cats', 'Posuere sollicitudin aliquam ultrices sagittis'),
-('The most incredible article about cats you''ll ever read', 'Here it is.'),
-('10 ways cats can help you live to 100', 'Posuere sollicitudin aliquam ultrices sagittis orci a.'),
-('9 reasons you can blame the recession on cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
-('10 ways marketers are making you addicted to cats', 'Posuere sollicitudin aliquam ultrices sagittis orci a.'),
-('11 ways investing in cats can make you a millionaire', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit');
+('5 life lessons learned from cats', 'Lorem ipsum dolor sit amet.', 100),
+('What the government doesn''t want you to know about cats', 'They''re cute!', 101),
+('The most boring article about cats you''ll ever read', 'Boring article', 102),
+('7 things lady gaga has in common with cats', 'Posuere sollicitudin aliquam ultrices sagittis', 103),
+('The most incredible article about cats you''ll ever read', 'Here it is.', 100),
+('10 ways cats can help you live to 100', 'Posuere sollicitudin aliquam ultrices sagittis orci a.', 103),
+('9 reasons you can blame the recession on cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 100),
+('10 ways marketers are making you addicted to cats', 'Posuere sollicitudin aliquam ultrices sagittis orci a.', 100),
+('11 ways investing in cats can make you a millionaire', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 100);
+
---
+++ routes/folders.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const knex = require('../knex');
+
+router.get('/', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('folders')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  knex
+    .select()
+    .from('folders')
+    .where('folders.id', `${id}`)
+    .returning('folders.id')
+    .then(result => {
+      if (result) {
+        res.json(result[0]);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateableFields = ['name'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  /***** Never trust users - validate input *****/
+  if (!updateObj.name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  console.log(updateObj);
+
+  knex('folders')
+    .where('folders.id', `${id}`)
+    .update(updateObj)
+    .returning('name')
+    .then(item => {
+      if (item) {
+        res.json(item[0]);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  const newItem = { name };
+  /***** Never trust users - validate input *****/
+  if (!newItem.name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  knex('folders')
+    .returning(['id', 'name'])
+    .insert(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  knex('folders')
+    .where({id: `${id}`})
+    .del()
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
